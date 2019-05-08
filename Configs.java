0467474137031cf1b84718144bb47ef1e94c82b6
nesrine <43906541+nesrinebnh@users.noreply.github.com>
2019-05-08 13:50:20 +0200

    Package DBConnection

---
+++ Configs.java
@@ -0,0 +1,12 @@
+package DBConnection;
+
+public class Configs {
+
+	protected static String dbHost = "localhost";
+	protected static String dbPort = "3306";
+	protected static String dbUser = "root";
+	protected static String dbPass = "";
+	protected static String dbName = "dbConnection";
+
+
+}
